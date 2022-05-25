eae0522882719aa49ad06ddf0bea9b0a3f91d4a1
JJuan <jjuanh@esliceu.net>
2022-05-25 08:42:39 +0200

    revisió tomeu

--- includes/header.php
+++ includes/header.php
@@ -5,5 +5,6 @@
         <a href="formulari.php">Formulari</a>
         <a href="proveidors.php">Proveidors</a>
         <a href="categories.php">Categorias</a>
+        <a href="ventes.php">Ventes</a>
     </body>
  </html>
---
+++ ventes.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php include "includes/head.php";
+    ?>
+    <?php include "includes/header.php";
+    ?>
+
+
+</head>
+
+<body>
+<section>
+        <table style="background-color:#000066;
+    color: white;" class="table">
+            <thead>
+                <tr>
+                    <td>
+                        <h3>ID</h3>
+                    </td>
+                    <td>
+                        <h3>Nom</h3>
+                    </td>
+                    <td>
+                        <h3>IDVenta</h3>
+                    </td>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $query = "SELECT producte.idproducte AS id, producte.nom, VENTA_PRODUCTE.fkidventa, VENTA_PRODUCTE.fkidproducte
+                FROM producte
+                INNER JOIN VENTA_PRODUCTE
+                WHERE producte.idproducte = VENTA_PRODUCTE.fkidproducte";
+                $result = mysqli_query($dbh, $query);
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr>, 
+                 <td>" . $row['id'] . "</td>
+                 <td>" . $row['nom'] . "</td>
+                 <td>" . $row['fkidventa'] . "</td>
+                 </tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+
+</body>
+
+</html>
